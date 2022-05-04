1ffa05a617469621160fae2accbd9be6183a5ad6
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-05-04 16:33:40 +0530

    Export enums

---
+++ src/models/index.ts
@@ -0,0 +1,16 @@
+import {
+    ButtonSize, ButtonStyle, 
+    ButtonType
+} from './Button';
+import { AnimationType } from './Animation';
+import { 
+    FontSize, TextStyle, 
+    TextInputStyle 
+} from './Text';
+
+export {
+    ButtonSize, ButtonStyle,
+    ButtonType, AnimationType,
+    FontSize, TextStyle, 
+    TextInputStyle
+}
