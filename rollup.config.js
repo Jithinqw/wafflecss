c70f97f673c40ccc59d7fa6bb1a515b53c7c56d6
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-05-14 20:31:37 +0530

    init

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import autoprefixer from 'autoprefixer';
 import filesize from 'rollup-plugin-filesize';
@@ -16,27 +17,30 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
+        sourcemap: true,
       },
       {
         file: packageJson.module,
         format: "esm",
-      },
-      {
-        file: packageJson.module,
-        format: "es",
-      },
+        sourcemap: true,
+      }
     ],
     plugins: [
       resolve(),
       commonjs(),
       peerDepsExternal(),
-      json({compact: true}),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      json({
+        compact: true
+      }),
+      typescript({ 
+        tsconfig: "./tsconfig.json"
+      }),
       filesize(),
       postcss({
             writeDefinitions: true,
             plugins:[autoprefixer],
-        })
+      }),
+      terser()
     ],
     external: [
       'react',
@@ -46,6 +50,7 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.scss$/],
     plugins: [dts()],
   },
 ];
