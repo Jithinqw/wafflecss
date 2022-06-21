6a5120d8cad94ef3271b5d73e784b3723b0be88a
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-06-21 23:58:58 +0530

    Improved API

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { HeadingType } from './models/Heading';
 export { default as Button } from './components/Button/Button';
 export { default as Heading } from './components/Heading/Heading';
 export { default as LegalText } from './components/Heading/LegalText/LegalText';
-export { default as TimerClock} from './components/Clock/Clock';
-export { default as StackedImage} from './components/Image/StackedImage/StackedImage';
+export { default as TimerClock } from './components/Clock/Clock';
+export { default as StackedImage } from './components/Image/StackedImage/StackedImage';
 export { default as Avatar } from './components/Image/Avatar/Avatar';
 export { default as Counter } from './components/Label/Counter/Counter';
 
