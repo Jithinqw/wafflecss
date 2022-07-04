98d457ac8de52851816575419116e8365d80548f
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-07-04 16:02:33 +0530

    API for pin input

---
+++ src/stories/PinInput.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import PinInput from '../components/Basic/Input/PinInput/PinInput';
+
+export default {
+    title: 'PinInput',
+    component: PinInput,
+    argTypes: {},
+    args: {
+        data: {
+            placeholder: 'TextInput Placeholder',
+            value: '2'
+        },
+        options: {
+            size: 'lg'
+        }
+    },
+} as ComponentMeta<typeof PinInput>
+
+const TextPinTemplate: ComponentStory<typeof PinInput> = (args) => {
+    return <PinInput {...args} />
+};
+
+export const DefaultPinInput = TextPinTemplate.bind({});
