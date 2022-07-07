cbf922be5581e9fb18e7eb321181f2b886cfc9a4
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-07-07 15:12:57 +0530

    API changes for StarRating

--- src/components/StarRating/StarRating.props.ts
+++ src/components/StarRating/StarRating.props.ts
@@ -1,6 +1,7 @@
 export interface IStarRatingProps {
     maxStarNum: number;
     defaultColor: string;
+    defaultStarSelection?: number;
     highlightColor: string;
     size?: 'sm' | 'md' | 'lg';
     onClick?: (starNumber: number) => void;
--- src/stories/StarRating.stories.tsx
+++ src/stories/StarRating.stories.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import StarRating from '../components/StarRating/StarRating';
+import { action } from "@storybook/addon-actions";
 
 export default {
     title: 'StarRating',
     component: StarRating,
     args: {
         maxStarNum: 5,
-        size: 'sm'
+        size: 'sm',
+        defaultStarSelection: 3
     },
 } as ComponentMeta<typeof StarRating>
 
 const StarRateTemplate: ComponentStory<typeof StarRating> = (args) => {
-    return (
-        <StarRating 
-            {...args} 
-        />
-    )
+    return <StarRating {...args} />
 };
 export const StarRatingTemplate = StarRateTemplate.bind({});
+
+
+const StarRatingEventTemplate: ComponentStory<typeof StarRating> = (args) => {
+    return <StarRating {...args} onClick={
+        action('User clicked on star rating')
+    }/>
+}
+
+export const StarRatingEventTest = StarRatingEventTemplate.bind({});
