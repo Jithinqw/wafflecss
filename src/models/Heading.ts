4f7192b6fd9027decc7a5e9f84eeffa402a0278e
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2022-07-14 17:36:36 +0530

    Unit test, prettier

--- src/models/Heading.ts
+++ src/models/Heading.ts
@@ -4,5 +4,5 @@ export enum HeadingType {
     H3 = 'h3',
     H4 = 'h4',
     H5 = 'h5',
-    H6 = 'h6'
+    H6 = 'h6',
 }
--- src/models/defaultStyle.ts
+++ src/models/defaultStyle.ts
@@ -5,56 +5,56 @@ export interface IStyleOptions {
     /**
      * background color for an ReactNode
      */
-    backgroundColor?: string;
+    backgroundColor?: string
     /**
      * color for an ReactNode
      */
-    color?: string;
+    color?: string
     /**
      * border radius for an ReactNode
      */
-    borderRadius?: string;
+    borderRadius?: string
     /**
      * font family for an ReactNode
      */
-    fontFamily?: string;
+    fontFamily?: string
     /**
      * Font weight for an ReactNode
      */
-    fontWeight?: string;
+    fontWeight?: string
     /**
      * font size for an ReactNode
      */
-    fontSize?: string;
+    fontSize?: string
 }
 
 /**
- * Image style and attibutes 
+ * Image style and attibutes
  * for an image.
  */
 export interface ImageData {
     /**
      * Image source
      */
-    imageSource: string;
-    height?: number;
-    width?: number;
-    alt?: string;
-    onClick?:() => void;
+    imageSource: string
+    height?: number
+    width?: number
+    alt?: string
+    onClick?: () => void
 }
 
 /**
  * DropDown attributes
  */
 export interface ISelectOption {
-    displayText: string;
-    value: string;
+    displayText: string
+    value: string
 }
 
 /**
  * Interface for Checkbox
  */
 export interface ICheckboxData {
-    displayText: string;
+    displayText: string
     value: string
 }
